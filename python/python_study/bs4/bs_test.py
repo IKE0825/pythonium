a05105818c9dad5234c692835d4f9019351d6c55
IKE0825 <itoken0825@gmail.com>
2020-01-27 11:45:48 +0900

    bs4 create

--- python/python_study/bs4/bs_test.py
+++ python/python_study/bs4/bs_test.py
@@ -1,4 +1,12 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs4
 
-url = ""
+url = "https://used.dospara.co.jp/sale/search.php?view=1&br=11&cbr=1102&page=1"
+
+res = requests.get(url)
+soup = bs4(res.text)
+
+dls = soup.find_all("dl")
+
+for dl in dls:
+    print(dl.text)
