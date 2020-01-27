175833e2c82959541d30a75871d54ff3cd85a058
IKE0825 <itoken0825@gmail.com>
2020-01-27 13:56:42 +0900

    bs4 -> selenium

--- python/python_study/selenium/python_libl.py
+++ python/python_study/selenium/python_libl.py
@@ -7,12 +7,20 @@ url = "https://docs.python.org/ja/3/library/index.html"
 res = requests.get(url)
 soup = bs4(res.content,"html.parser")
 
-uls = soup.find_all("ul")
+uls = soup.find_all(class_ = "toctree-l2")
+links = soup.find_all(class_ = "reference internal")
 
 lst_uls = []
+lst_link = []
 
 for ul in uls:
     #print(ul.text)
     lst_uls.append(ul.text)
 
-print(lst_uls)
+for link in links:
+    url = link.get("href")
+    lst_link.append(url)
+
+#print(lst_uls)
+
+print(lst_link)
