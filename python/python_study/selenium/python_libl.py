098015b22b0da9056f42c3f929666fdb867431c4
IKE0825 <itoken0825@gmail.com>
2020-01-28 09:40:11 +0900

    200128 commit

--- python/python_study/selenium/python_libl.py
+++ python/python_study/selenium/python_libl.py
@@ -15,27 +15,48 @@ import requests
 #　※Chromedriverが無いとseleniumuは動かない
 #　下記パスにあるchromedriver.exeをディレクトリごとコピー
 
+#リンク取得
 res = requests.get("https://docs.python.org/ja/3/library/index.html")
 soup = BeautifulSoup(res.content,"html.parser")
 
 links = soup.find_all(class_ = "reference internal")
-linkText = links[2].text
-print(linkText)
 
+nmlinks = len(links)
+print(nmlinks)
+
+lstLink =[]
+
+for i in range(nmlinks):
+    linkElem = links[i].text
+    lstLink.append(linkElem)
+    #linkText = links[].text
+    #print(linkText)
+
+print(lstLink)
+
+#ブラウザ自動操作
 path = r"C:\python\chromedriver\chromedriver.exe"
+
 #linuxの場合はpath指定不要
 #binファイル内にchromedriverを格納 "sudo mv chromedriver /usr/local/bin"
 #おまじない "sudo chown root:root /usr/local/bin/chromedriver"
 
-#indowsの場合、pathを指定
-browser = webdriver.Chrome()
+#Windowsの場合、pathを指定
+browser = webdriver.Chrome(path)
 browser.get("https://docs.python.org/ja/3/library/index.html")
 
 urls = []
 
 time.sleep(1)
 
-pdb.set_trace()
+#pdb.set_trace()
 
-element = browser.find_elements_by_link_text(linkText)
-print(element)
+for i in range(nmlinks):
+    selLink = lstLink[i]
+    element = browser.find_element_by_link_text(selLink)
+    element.click()
+    cur_url = browser.current_url
+    urls.append(cur_url)
+    browser.back()
+
+print(urls)
